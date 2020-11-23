09f29e6152549f28eb4396dbe25c0625486d46f2
sshridhar1965 <74544097+sshridhar1965@users.noreply.github.com>
2020-11-23 14:22:58 +0530

    Add files via upload

--- CalculatorW2D51.py
+++ CalculatorW2D51.py
@@ -1,7 +1,39 @@
-# LeapYearW2D52
-print("Please Enter the Year")
-Year=int(input())
-if Year%4==0:
-    print ("This is a Leap Year")
-else:
-    print (" This is not a Leap Year")
+
+# CalculatorW2D51
+
+print("Please select from following options")
+print("1 - Addition")
+print("2 - Subtraction")
+print("3 - Multiplication")
+print("4 - Division")
+print("5 - Table of")
+function=int(input())
+if (function==1):
+    
+    a=input("Please Enter the first number")
+    b=input("Please Enter the second number")
+    c=int(a)+int(b)
+    print("The sum is = ",c)
+if (function==2):
+    
+    a=input("Please Enter the first number")
+    b=input("Please Enter the second number")
+    c=int(a)-int(b)
+    print("The difference is = ",c)
+if (function==3):
+    
+    a=input("Please Enter the first number")
+    b=input("Please Enter the second number")
+    c=int(a)*int(b)
+    print("The product is = ",c)
+if (function==4):
+    
+    a=input("Please Enter the first number")
+    b=input("Please Enter the second number")
+    c=int(a)/int(b)
+    print("The result is = ",c)
+if (function==5):
+    
+    a=input("Please Enter the number for which you want the table")
+    for i in range(1,11):
+        print(int(a)*i)

cb33e45fa6f54d760aa5e2e7714ac200ee8f2fe8
sshridhar1965 <74544097+sshridhar1965@users.noreply.github.com>
2020-11-24 16:17:10 +0530

    Add files via upload

---
+++ SumNatNumbers.py
@@ -0,0 +1,8 @@
+
+# sum of nat numbers
+num = int(input("Please Enter any positive natural number"))
+sum=0
+while (num>=1):
+    sum = num+sum
+    num = num-1
+print(sum)
---
+++ VowelsW3D1.py
@@ -0,0 +1,7 @@
+
+# sum of nat numbers
+num = int(input("Please Enter any positive natural number"))
+sum=0
+while (num<=1):
+    sum = num+sum
+    num-= 1

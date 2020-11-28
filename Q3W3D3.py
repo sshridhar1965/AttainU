091eb10be5ac67c6cfdf2a777100c62119e900d6
sshridhar1965 <74544097+sshridhar1965@users.noreply.github.com>
2020-11-28 13:50:09 +0530

    Add files via upload

---
+++ Q3W3D3.py
@@ -0,0 +1,14 @@
+
+# driver speed
+def speed():
+    a=int(input(" Please enter the speed"))
+    points=0
+    if(a<=70):
+        print ("OK")
+    else:
+        (a>70)
+        points=(a-70)//5
+        print ("Points =", points)
+    if(points>12):
+        print("Licence Suspended")
+speed()

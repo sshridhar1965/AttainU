aeb5a5fa3e58ecd74c9d2cf66424da13cd0f1ca3
sshridhar1965 <74544097+sshridhar1965@users.noreply.github.com>
2020-11-28 12:24:54 +0530

    Add files via upload

---
+++ Q1W3D3.py
@@ -0,0 +1,11 @@
+
+# Maximum of two numbers
+a=input(" Please enter first number")
+b=input("Please enter second number")
+if(a>b):
+    print ("First number is maximum")
+elif(a==b):
+    print ("First number equals second number")
+else:
+    print("Second number is maximum")
+

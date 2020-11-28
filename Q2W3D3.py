18aeaaad1146104dffd2ee357e98c29dadef4034
sshridhar1965 <74544097+sshridhar1965@users.noreply.github.com>
2020-11-28 12:39:26 +0530

    Add files via upload

---
+++ Q2W3D3.py
@@ -0,0 +1,12 @@
+
+# FizzBuzz
+a=int(input(" Please enter the number"))
+
+if(a%3==0 and a%5==0):
+    print ("FizzBuzz")
+elif(a%3==0):
+    print ("Fizz")
+elif(a%5==0):
+    print("Buzz")
+else:
+    print(a)
